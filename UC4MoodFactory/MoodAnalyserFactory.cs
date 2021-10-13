e50694370fd91230c5c36fbc671f15a74e45173f
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-13 12:36:16 +0530

    Hemanth : Mood Analyser With default Constructor

---
+++ UC4MoodFactory/MoodAnalyserFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UC4MoodFactory
+{
+    class MoodAnalyserFactory
+    {
+        private string message;
+        public MoodAnalyserFactory(string message)
+        {
+            this.message = message;
+        }
+
+        // UC 4
+        public static object CreateMoodAnalyser(string className, string constructorName)
+        {
+            string pattern = @"." + constructorName + "$";
+            Match result = Regex.Match(className, pattern);
+            if (result.Success)
+            {
+                try
+                {
+                    Assembly executing = Assembly.GetExecutingAssembly();
+                    Type moodAnalyseType = executing.GetType(className);
+                    return Activator.CreateInstance(moodAnalyseType);
+                }
+                catch (ArgumentNullException)
+                {
+                    throw new CException(CException.ExceptionType.no_such_class, "Class not found");
+                }
+            }
+            else
+            {
+                throw new CException(CException.ExceptionType.no_such_method, "Constructor is not found");
+            }
+        }
+
+       
+    }
+}
---
+++ UC4MoodFactory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UC4MoodFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
